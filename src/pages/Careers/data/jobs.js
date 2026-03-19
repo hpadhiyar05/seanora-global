50c44dd46053fb59415c8c4cb92da8a4a9bbc5a9
Mihir Patel <mihirbhalani18@gmail.com>
2026-03-19 23:38:10 +0530

    add jobs data constats

---
+++ src/pages/Careers/data/jobs.js
@@ -0,0 +1,25 @@
+export const CATEGORIES = [
+  'View all',
+  'Development',
+  'Design',
+  'Marketing',
+  'Customer Service',
+  'Operations',
+  'Finance',
+  'Management',
+];
+
+export const JOB_TYPES = [
+  'All Types',
+  'Full-time',
+  'Part-time',
+  'Contract',
+  'Internship',
+];
+
+export const LOCATIONS = [
+  'All Locations',
+  '100% Remote',
+  'Hybrid',
+  'On-site',
+];
